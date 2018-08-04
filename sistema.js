1864d96e3e62e03b1ba5fba4583c46a704f97fc3
lusobr <leogonsil97@gmail.com>
2018-08-03 23:11:20 -0300

    HTML participantes sem tabela com estilo CSS

--- sistema.js
+++ sistema.js
@@ -4,19 +4,19 @@ function Participante() {
     this.sobrenome = "";
     this.email = "";
     this.idade = 0;
-    this.sexo = 0
-    this.nota = 0
-    this.aprovado = false
+    this.sexo = 0;
+    this.nota = 0;
+    this.aprovado = false;
 }
 
 
- // Representa o sistema
- 
+// Representa o sistema
+
 function SistemaCadastro() {
     var participantes = [];
-    
+
     function adicionarParticipante(nome, sobrenome, email, idade, sexo) {
-        if(obterParticipante(email) === undefined){
+        if (obterParticipante(email) === undefined) {
             var p = new Participante();
             p.nome = nome;
             p.sobrenome = sobrenome;
@@ -25,83 +25,86 @@ function SistemaCadastro() {
             p.sexo = sexo;
 
             participantes.push(p);
-        }else{
+        } else {
             throw 'Participante já existente!';
         }
     }
-    
+
     function removerParticipante(email) {
-        participantes.splice(participantes.findIndex(function (objetoParticipante){
+        participantes.splice(participantes.findIndex(function (objetoParticipante) {
             return (objetoParticipante.email === email);
-        }),1);
-    }      
-    
-    function buscarParticipantesPorNome(nome){
-        return participantes.filter(function(objetoParticipante){ 
-            return objetoParticipante.nome === nome;  
+        }), 1);
+    }
+
+    function buscarParticipantesPorNome(nome) {
+        return participantes.filter(function (objetoParticipante) {
+            return objetoParticipante.nome === nome;
         });
 
-    }    
-    
-    function buscarParticipantesPorSexo(sexo){
-        return participantes.filter(function(objetoParticipante){
+    }
+
+    function buscarParticipantesPorSexo(sexo) {
+        return participantes.filter(function (objetoParticipante) {
             return objetoParticipante.sexo === sexo;
         });
-    
+
     }
-   
-    function buscarParticipantesAprovados(){
-        return participantes.filter(function(objetoParticipante){   
-            if(objetoParticipante.aprovado){
+
+    function buscarParticipantesAprovados() {
+        return participantes.filter(function (objetoParticipante) {
+            if (objetoParticipante.aprovado) {
                 return objetoParticipante;
-            }  
+            }
         });
     }
-   
-    function buscarParticipantesReprovados(){  
-        return participantes.filter(function(objetoParticipante){           
-            if(objetoParticipante.aprovado === false){
+
+    function buscarParticipantesReprovados() {
+        return participantes.filter(function (objetoParticipante) {
+            if (objetoParticipante.aprovado === false) {
                 return objetoParticipante;
-            }  
+            }
         });
     }
-    
-    function obterParticipante(email){      
-        return participantes.find(function(objetoParticipante){          
+
+    function obterParticipante(email) {
+        return participantes.find(function (objetoParticipante) {
             return objetoParticipante.email === email;
         });
-        
+
     }
-   
-    function adicionarNotaAoParticipante(email,nota){          
-       index = participantes.findIndex(function(objetoParticipante){
-           return objetoParticipante.email === email;
-       });
-       participantes[index].nota = nota;
-       if(participantes[index].nota >= 70)
+
+    function adicionarNotaAoParticipante(email, nota) {
+        index = participantes.findIndex(function (objetoParticipante) {
+            return objetoParticipante.email === email;
+        });
+        participantes[index].nota = nota;
+        if (participantes[index].nota >= 70)
             participantes[index].aprovado = true;
-       else
+        else
             participantes[index].aprovado = false;
-    }  
-   
-    function obterMediaDasNotasDosParticipantes(){ 
-        return participantes.reduce(function(somas,objetoParticipante){    
-            return somas + objetoParticipante.nota;
-        },0)/ participantes.length;
     }
-   
-    function obterTotalDeParticipantes(){       
+
+    function obterMediaDasNotasDosParticipantes() {
+        return participantes.reduce(function (somas, objetoParticipante) {
+            return somas + objetoParticipante.nota;
+        }, 0) / participantes.length;
+    }
+
+    function obterTotalDeParticipantes() {
         return participantes.length;
     }
-   
-    function verificarSeParticipanteEstaAprovado(email){       
-            return obterParticipante(email).aprovado;
+
+    function verificarSeParticipanteEstaAprovado(email) {
+        return obterParticipante(email).aprovado;
     }
-    
-    function obterQuantidadeDeParticipantesPorSexo(sexo){        
+
+    function obterQuantidadeDeParticipantesPorSexo(sexo) {
         return buscarParticipantesPorSexo(sexo).length;
     }
 
+    function imprimirParticipantes() {
+        return participantes;
+    }
     return {
         adicionarParticipante,
         removerParticipante,
@@ -114,6 +117,6 @@ function SistemaCadastro() {
         obterMediaDasNotasDosParticipantes,
         obterTotalDeParticipantes,
         verificarSeParticipanteEstaAprovado,
-        obterQuantidadeDeParticipantesPorSexo    
+        obterQuantidadeDeParticipantesPorSexo
     };
 }
